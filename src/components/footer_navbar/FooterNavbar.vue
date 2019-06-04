ba13d0252fad01ba402cba2c8240ea5eaa77133b
Jesús Barrientos <jesus.barrientosg.95@gmail.com>
2019-06-04 01:06:11 -0400

    Updated Budget view

--- src/components/footer_navbar/FooterNavbar.vue
+++ src/components/footer_navbar/FooterNavbar.vue
@@ -1,8 +1,8 @@
 <template>
     <div id="footer-navbar-component">
-        <navbar-icon title="Dashboard" icon="dashboard" url="dashboard" />
-        <navbar-icon title="Módulos" icon="appstore" url="modules" />
-        <navbar-icon title="Cuenta" icon="user" url="account" />
+        <navbar-icon title="Dashboard" icon="dashboard" url="/dashboard" />
+        <navbar-icon title="Módulos" icon="appstore" url="/modules" />
+        <navbar-icon title="Cuenta" icon="user" url="/account" />
     </div>
 </template>
 
